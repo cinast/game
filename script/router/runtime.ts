35a8b71dea1276b1ae3a1cd42a2764c44c47b3f5
cinast <126345646+cinast@users.noreply.github.com>
2024-02-08 14:14:54 +0000

    add _runtime_.router

---
+++ script/router/runtime.ts
@@ -0,0 +1,7 @@
+export * from "../source/character"
+export * from "../source/basic"
+export * from "../source/events"
+export * from "../source/sense"
+export * from "../source/utils/ui"
+export * from "../source/utils/utils"
+ 

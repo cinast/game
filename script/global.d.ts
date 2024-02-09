685b394f26a6b0f9fdf963b4eb66e9cbfdb08a6b
cinast <126345646+cinast@users.noreply.github.com>
2024-02-09 13:26:02 +0000

    omg, sh*ttr d.ts

---
+++ script/global.d.ts
@@ -0,0 +1,77 @@
+declare const version = "0.0.0";
+declare const baseurl = "https://github.com/cinast/game/blob/main/";
+
+declare type K = string | number | symbol
+
+declare const world: {
+    characterkist: character[],
+    sense: sense[],
+    time: number,
+    date: Date,
+}
+
+class sense {
+    readonly id: string
+    readonly BaseType: "sense";
+    type: string
+    tag: string[]
+    Idnex
+    characters: Record<string, character>;
+    addCharacter(...character: character[]): void
+    remove() { }
+}
+
+
+declare class basicObject {
+    readonly id: string;
+    readonly BaseType: string
+    type: string
+    tag: string[]
+    constructor(id?: string)
+}
+
+
+declare class character extends basicObject {
+    BaseType: "character";
+    x: number
+    y: number
+    name: string
+    globalSize: number
+    visitable: boolean
+    rotation: number
+    isClone: boolean
+    cloneNumber: number
+    hasCloned: number
+    clones: clonedCharacter[]
+    CloneFrom: character | undefined;
+    effects;
+    layerset: Layer[];
+    eventList: Record<string, eventObjet>;
+    moveto(x: number, y: number): void
+    clone(): clonedCharacter
+    set cloneFrom(char: character | undefined)
+    addEvListener(type: string, callback: Function): string
+    deteleEvListener(evID: string, replace?: eventObjet): void
+}
+
+declare class clonedCharacter extends character {
+    isClone: boolean
+    set cloneFrom(char: character | undefined)
+    constructor(baseChara: character | clonedCharacter)
+}
+
+declare class Layer {
+    readonly id: string;
+    readonly BaseType: string
+    type: string
+    tag: string[]
+    name: string
+    layerIndex: number
+    // content: ImageData;
+    parent: character | undefined
+    constructor(type?: string, size?: number, id?: string)
+    moveto(index: number): void
+    delete(): Layer | undefined | never
+    concatLayer() { }
+}
+
--- script/runtime/main.ts
+++ script/runtime/main.ts
@@ -2,5 +2,9 @@ console.log("-main-")
 import * as u from "../router/runtime";
 
 let rc = new Worker("./rander")
-rc.onmessage((a,b)=>{})
+
+
+const world = {
+    
+}
 

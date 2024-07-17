7d3cdd2bdb4f5a1386b37c0403bee2e3691da8db
cinast <pastfor@outlook.com>
2024-07-17 21:27:24 +0800

    。

--- source/router/chata.js
+++ source/router/chata.js
@@ -1,21 +1,17 @@
-/*    ---- 7.10 ----
+/*
 
-
- c.: 啊啊啊  item.effect 和 basething.event 概念搞混了啊啊
-
-
-
+---- 7.10 ----
+c.: 啊啊啊 item.effect 和 basething.event 概念搞混了啊啊
 
     ---- 7.11 ----
- c.: 哈欠
 
+c.: 哈欠
 
-   --- 7.12 ----
- c.: 你们几个吧信息在package.json李完善一下，邮箱的
+--- 7.12 ----
+c.: 你们几个吧信息在 package.json 李完善一下，邮箱的
 
-
-   --- 7.18 ---
- p.: 
+--- 7.18 ---
+p.:
 
     nnnnnnnn     oooooo   dddddddd   eeeeeeeeee
     nnnn  nnnn oooo  oooo dddd  dddd eeeeeeeeee
@@ -24,26 +20,46 @@
     nnnn  nnnn oooo  oooo dddd  dddd ee                  jj   ss
     nnnn  nnnn oooo  oooo dddd  dddd eeeeeeeeee .... jj  jj     ss
     nnnn  nnnn   oooooo   dddddddd   eeeeeeeeee ....   jj   ssss
-    
-{"imgood":true, "[]}
 
-c.: 看12号消息
-    我趣，ai还开着想帮我打字（
-    
+c.: 看 12 号消息
+    我趣，ai 还开着想帮我打字（
+    所有人发言往下发，带自己名字
+    缩进用 tab
+    我靠 blackboxai 说中文
+    怎么自动缩进是个 html 的缩进
+s.:
+    Languages:  
+    在 js 里面的注释也算 js 吧
+
+p.: 这下成纯文本文件了
+    向服务器大量发包:angry:
+
+c.: @paskaw 再瞎搞你 readonly
+
+p.: :sob::sob::sob::sob:
+    憋玩了，东西不弄了吗
+
+c.: 等会我有点分不清你两（）
+    @px 你是 s 对吧（
+
+p.: 似乎纯文本算 others
+    m   d 又不是纯文本哦哦噢噢噢噢
+
+c.: markdown 吧你们
+    美观（）
+    可恶没有预览
+    我改
 
 
+p.: 装一个
+    不  行的话就把名字换回来，那个 js 文件还在我工作区里
 
+c.: 啊？选择+tab = 集体缩进？
 
+p.: 你们不去我去学代码了
+    对啊？你现在才知道嘛
 
+c.: 这个文件我在 package & tscfg 里排除了
+    但是git没有
 
-
-
-
-
-
-
-
-
-
-    
 */
